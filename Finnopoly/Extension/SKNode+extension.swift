ce85e65fe47399acd55801a93377bfc6c478207b
bill0930 <bill0930@gmail.com>
2019-10-24 15:37:03 +0800

    use Graph data strcutre to store the routeMap

--- Finnopoly/Extension/SKNode+extension.swift
+++ Finnopoly/Extension/SKNode+extension.swift
@@ -8,26 +8,39 @@
 
 import SpriteKit
 extension SKNode {
-
-//unarchive a sks file then turn it to a Node
-class func unarchiveFromFile(file: String) -> SKNode? {
-    if let path = Bundle.main.path(forResource:file, ofType: "sks"){
-        let url = URL(fileURLWithPath: path)
-        do{
-            let sceneData = try Data(contentsOf: url, options: .mappedIfSafe)
-            let archiver =  try NSKeyedUnarchiver(forReadingFrom: sceneData)
-            archiver.requiresSecureCoding = false
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as! SKNode
-            archiver.finishDecoding()
-            return scene
-        } catch {
-            print(error.localizedDescription)
+    
+    /// unarchiveFromFile is the function to accept  sks file parametre and add it into the scene and become a SKNode
+    class func unarchiveFromFile(file: String) -> SKNode? {
+        if let path = Bundle.main.path(forResource:file, ofType: "sks"){
+            let url = URL(fileURLWithPath: path)
+            do{
+                let sceneData = try Data(contentsOf: url, options: .mappedIfSafe)
+                let archiver =  try NSKeyedUnarchiver(forReadingFrom: sceneData)
+                archiver.requiresSecureCoding = false
+                archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+                let scene = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as! SKNode
+                archiver.finishDecoding()
+                return scene
+            } catch {
+                print(error.localizedDescription)
+                return nil
+            }
+            
+        }else {
             return nil
         }
-        
-    }else {
-        return nil
     }
-}
+    
+    /// getCountWithName is the function to accept name parametre and returns the child node count with that name inclusive
+    /// e.g if the node testNode has children node of brown0, brown1, brown2, testNode.getCountWithName(withName: "brown") will return 3
+    /// - parameter name: the string to be count,
+    func getCountWithName(withName name: String?) -> Int{
+        var count: Int = 0
+        self.enumerateChildNodes(withName: "*\(name!)*"){
+            node, _ in
+            count = count + 1
+        }
+        return count
+    }
+
 }
--- Finnopoly/GameScene.swift
+++ Finnopoly/GameScene.swift
@@ -11,12 +11,13 @@ import SwiftGraph
 
 class GameScene: SKScene {
     
-    var mapNode: SKNode!
-//    var brownLineStations: [Station] = []
-//    var redLineStations: [Station] = []
+    var stationVertices : [String] = []
+    var routeGraph: UnweightedGraph<String>!
+    var worldNode: SKNode!
     
     override func didMove(to view: SKView) {
         loadScene(filename: "GameScene")
+        getCityGraph()
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
@@ -32,17 +33,77 @@ class GameScene: SKScene {
     }
     
     func loadScene(filename: String){
-        if let routeMap = SKNode.unarchiveFromFile(file: filename){
-            mapNode = routeMap
-            addChild(mapNode)
-            for child in mapNode.children {
-                if child.name == "routeMap" {
-                    for station in child.children {
-//                        print(station.name)
-                        print("testingtesting")
+        if let world = SKNode.unarchiveFromFile(file: filename){
+            worldNode = world
+            addChild(worldNode)
+            
+            
+        }
+    }
+    
+    func getCityGraph(){
+        //        let stationGraph: WeightedGraph<structStation, Int> = WeightedGraph<structStation, Int>()
+        for child in worldNode.children {
+            if child.name == "routeMap" {
+                for station in child.children {
+                    
+                    if let tempName = station.name{
+                        stationVertices.append(tempName)
                     }
                 }
             }
         }
+        //assign to the Graph
+        routeGraph = UnweightedGraph<String>(vertices: stationVertices)
+        print(routeGraph.vertexCount)
+        
+        graphConnection()
     }
+    
+    ///to mapped the routeMap into Graph
+    func graphConnection(){
+        //initialize the connection between station
+        let lineColors = ["brown","orange","red","blue","purple","pink","cyan","grey","greenN","greenS"]
+        let mapNode = worldNode.childNode(withName: "routeMap")!
+        
+        for color in lineColors {
+            //            print(color)
+            var stationArray = [String]() //store the stations for particular color line
+            let nameCount: Int = mapNode.getCountWithName(withName: color)
+            
+            mapNode.enumerateChildNodes(withName: "*\(color)*"){ //help append all station into the Array with same color
+                node, _ in
+                stationArray.append(node.name!)
+            }
+            
+            ///brown0<->brown1<->brown2<->brown3<->brown4
+            /// for i in 0 to 3 so
+            ///brown0<->brown1, brown1<->brown2, brown3<->brown4
+            for i in 0...nameCount - 2 {
+                let fromIndex: Int = (stationArray.firstIndex(where: {$0.contains(color + String(i)) } )!)
+                let toIndex: Int = (stationArray.firstIndex(where: {$0.contains(color + String(i+1)) } )!)
+                routeGraph.addEdge(from: stationArray[fromIndex],to: stationArray[toIndex], directed: false)
+                //print(stationArray[fromIndex] + " is connected to " + stationArray[toIndex])
+            }
+            
+        }
+        /// for debugging
+        
+        //print out all the adjacent stations for  the statons name contains ("IC")
+        for v in routeGraph {
+            if v.contains("IC"){
+                print("stations \(v) : \(routeGraph.neighborsForVertex(v)!)")
+            }
+        }
+        
+        //print out the first path from "IC-red0-brown0" to "IC-blue5-purple4" using bfs
+        let result = routeGraph.bfs(from: "IC-red0-brown0") { (v) -> Bool in
+            v == "IC-blue5-purple4"
+        }
+        for edge in result {
+            print("\(routeGraph.vertexAtIndex(edge.u)) -> \(routeGraph.vertexAtIndex(edge.v))")
+        }
+        
+    }
+
 }
